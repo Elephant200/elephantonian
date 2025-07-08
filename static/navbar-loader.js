884f2ad7c5ca633d5075807822a61a4fc60147ea
Alex <alexktev@gmail.com>
2025-07-08 15:42:20 -0400

    Remove console logs

--- static/navbar-loader.js
+++ static/navbar-loader.js
@@ -9,18 +9,15 @@ document.addEventListener("DOMContentLoaded", function() {
         navbarPlaceholder.innerHTML = data;
         const dropdownItem = document.querySelector(`a.dropdown-item[href="${currentPage}"]`);
         if (dropdownItem) {
-            console.log("Found dropdown item")
             dropdownItem.classList.add('nav-link-current');
         }
         const navLink = document.querySelector(`a.nav-link[href="${currentPage}"]`);
         if (navLink) {
-            console.log("Found nav link")
             navLink.classList.add('nav-link-current');
         }
 
         const gamesLink = document.querySelector("a.nav-link[href='/games.html']");
         if (gamesLink && currentPage.includes("/games/")) {
-            console.log("Found games link")
             gamesLink.classList.add('nav-link-current');
         }
     });
